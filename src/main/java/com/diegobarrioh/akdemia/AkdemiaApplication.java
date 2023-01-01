ac61ded63e9a93ecfcb21eec3736f8050bd44e74
Diego Barrio H <diegobarrioh@gmail.com>
2023-01-01 23:51:18 +0100

    starting point. Init project, and structure

---
+++ src/main/java/com/diegobarrioh/akdemia/AkdemiaApplication.java
@@ -0,0 +1,13 @@
+package com.diegobarrioh.akdemia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkdemiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkdemiaApplication.class, args);
+	}
+
+}
